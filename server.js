cda5e345d7620683ba7b4a596b1858152dcf02b2
Valviesse <mateusvalviesse@gmail.com>
2022-11-03 21:59:37 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const jsonServer = require('json-server');
+import jsonServer from 'json-server';
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
+const router = jsonServer.router('db.json'); 
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const port = process.env.PORT || 3200; 
 
 server.use(middlewares);
+server.use(
+    jsonServer.rewriter({
+      '/:resource/list': '/:resource',
+      '/:resource/get/:id': '/:resource/:id',
+      '/:resource/create': '/:resource',
+      '/:resource/update/:id': '/:resource/:id',
+      '/:resource/delete/:id': '/:resource/:id',
+    }),
+  );
 server.use(router);
-
 server.listen(port);
